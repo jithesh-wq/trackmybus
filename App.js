40a5c6d087c32f20e7f911a9995a96108c8bf6be
jithesh-wq <jitheshtv4@gmail.com>
2021-06-01 22:06:41 +0530

    Co-authored-by: Pranav Kumar p <pranavekumarp@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
+import Login from "./screens/Login"
+import Signup from "./screens/Signup"
 const App = () => {
   return (
     <View>
-      <Text></Text>
+      <Login/>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+})
